66d29b7ba933d548d5a357ec4169d513c0056c7c
harshalmathur07 <harshalmathur2005@gmail.com>
2026-03-02 11:01:38 +0530

    Added gunicorn and port config

--- app.py
+++ app.py
@@ -18,3 +18,9 @@ app = create_app()
 @app.route("/")
 def home():
     return {"message": "Backend Server is running"}
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
